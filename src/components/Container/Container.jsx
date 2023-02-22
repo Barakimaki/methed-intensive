a822e630d196309cb88a4d1e680b81e7efeb0c89
Adamonis Arseni <arsenaloo97@gmail.com>
2023-02-22 22:20:50 +0300

    first day header footer main

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,9 +1,9 @@
 import style from './Container.module.css'
 import classNames from "classnames";
 
-export const Container = ({children, className}) => {
+export const Container = ({className, children}) => {
     return (
-        <div className={classNames(style.container, className)}>
+        <div className={classNames(className, style.container)}>
             {children}
         </div>
     );
